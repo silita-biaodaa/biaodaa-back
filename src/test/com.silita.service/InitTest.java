3089a624b5ffe1b006533f272e134c6737b12b88
gemingyi <915674992@qq.com>
2018-08-09 14:54:41 +0800

    标大大后台管理系统

---
+++ src/test/com.silita.service/InitTest.java
@@ -0,0 +1,17 @@
+package com.silita.service;
+
+import org.junit.Test;
+
+/**
+ * Create by IntelliJ Idea 2018.1
+ * Company: silita
+ * Author: gemingyi
+ * Date: 2018-08-09 11:44
+ */
+public class InitTest extends ConfigTest {
+
+    @Test
+    public void initTest() {
+        System.out.println("启动成功！！！");
+    }
+}
