090773980184250d374b93d0b66ff19426fdf1a9
gemingyi <915674992@qq.com>
2018-10-29 15:03:08 +0800

    标大大接口

---
+++ src/main/java/com/silita/dao/TbCompanyAwardsMapper.java
@@ -0,0 +1,7 @@
+package com.silita.dao;
+
+import com.silita.model.TbCompanyAwards;
+import com.silita.utils.MyMapper;
+
+public interface TbCompanyAwardsMapper extends MyMapper<TbCompanyAwards> {
+}
